a03e3f2de0095fe52a37ca0466380707af4750b5
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-10-07 12:07:40 +0200

    Fix the strings generation.

--- lib/icapps/translations/strings.rb
+++ lib/icapps/translations/strings.rb
@@ -33,14 +33,14 @@ module Icapps
         def fetch_language_strings_file(language)
           short_name = language['short_name']
           puts "[VERBOSE] Fetching #{short_name} translations.".colorize(:white) if options[:verbose]
-          string_files = Dir.glob("**/#{short_name}.lproj/#{}")
+          string_files = Dir.glob("**/#{short_name}.lproj/#{config.filename}")
           if string_files.count == 0
             puts "[WARNING] No 'Localizable.string' file found for the #{short_name} language.".colorize(:yellow)
           elsif string_files.count > 1
             puts "[WARNING] Multiple 'Localizable.string' files found for the #{short_name} language.".colorize(:yellow)
           else
             strings = ::Icapps::Translations::Http.authenticated_response("translations/#{language['id']}.strings")
-            write_to_strings_file strings, strings_files, language
+            write_to_strings_file strings, string_files, language
           end
         end
 
