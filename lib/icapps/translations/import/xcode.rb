a21381a9d75da0ade54f4b5c684ad39e9aa02fd0
Jelle Vandebeeck <jelle@fousa.be>
2016-01-18 11:18:59 +0100

    Ignore the strings files in the Build folder.

--- lib/icapps/translations/import/xcode.rb
+++ lib/icapps/translations/import/xcode.rb
@@ -11,7 +11,8 @@ module Icapps
           def fetch_language_file(language)
             short_name = language['short_name']
             puts "[VERBOSE] Fetching #{short_name} translations.".colorize(:white) if options[:verbose]
-            string_files = Dir.glob("**/#{short_name}.lproj/#{config.filename}")
+            # Ignore the .strings files in the Build folder.
+            string_files = Dir.glob("**/#{short_name}.lproj/#{config.filename}").reject{ |f| f[%r{^Build/}] }
             if string_files.count == 0
               puts "[WARNING] No 'Localizable.string' file found for the #{short_name} language.".colorize(:yellow)
             elsif string_files.count > 1
