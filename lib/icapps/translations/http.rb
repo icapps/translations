9a7789ce4598d9f7dd51e46d1ff8eb815604e358
Jelle Vandebeeck <jelle@fousa.be>
2018-04-27 10:58:24 +0200

    Fix SSL without an error.

--- lib/icapps/translations/http.rb
+++ lib/icapps/translations/http.rb
@@ -8,14 +8,15 @@ module Icapps
     class Http
       class << self
         def authenticated_response(path, is_json = false)
-          OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
           uri = URI("#{config.url}/#{path}")
           puts "[VERBOSE] Connecting to url '#{uri}'.".colorize(:white) if options[:verbose]
 
           http = Net::HTTP.new(uri.host, uri.port)
           http.use_ssl = uri.scheme == 'https'
-          request =  Net::HTTP::Get.new(uri)
+          # Disable SSL for now.
+          ssl_context = OpenSSL::SSL::SSLContext.new
+          ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          request =  Net::HTTP::Get.new(uri, ssl_context: ssl_context)
           request.add_field 'Authorization', "Token token=#{config.project_key}"
           response = http.request(request)
           is_json ? JSON.parse(response.body) : response.body
