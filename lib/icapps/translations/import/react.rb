fa6e476fb9d383e82c84c6bd0da9f12780f3c10a
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2018-07-25 16:08:51 +0200

    Make sure we right it correct

--- lib/icapps/translations/import/react.rb
+++ lib/icapps/translations/import/react.rb
@@ -11,6 +11,14 @@ module Icapps
           def fetch_language_file(language)
             short_name = language['short_name']
             puts "[VERBOSE] Fetching #{short_name} translations.".colorize(:white) if options[:verbose]
+            json_files = Dir.glob("labels/#{short_name}.json")
+
+            if json_files.count > 1
+              puts "[WARNING] Multiple '#{short_name}.json' files found for the #{short_name} language.".colorize(:yellow)
+            else
+              json = ::Icapps::Translations::Http.authenticated_response("translations/#{language['id']}.json")
+              write_to_file json, json_files, language
+            end
           end
         end
       end
